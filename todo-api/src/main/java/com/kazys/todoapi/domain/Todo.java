3e53d3c1b81a5a1bb269d681900637f007092087
Kazys Kersulis <kazimieras.kersulis@gmail.com>
2019-01-29 23:01:24 +0200

    added Lombok annotation to Todo model class

--- todo-api/src/main/java/com/kazys/todoapi/domain/Todo.java
+++ todo-api/src/main/java/com/kazys/todoapi/domain/Todo.java
@@ -1,53 +1,18 @@
 package com.kazys.todoapi.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Todo {
 
     private long id;
     private String content;
     private boolean archived;
     private Date created;
-
-    public Todo() {
-    }
-
-    public Todo(long id, String content, boolean archived, Date created) {
-        this.id = id;
-        this.content = content;
-        this.archived = archived;
-        this.created = created;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public boolean isArchived() {
-        return archived;
-    }
-
-    public void setArchived(boolean archived) {
-        this.archived = archived;
-    }
-
-    public Date getCreated() {
-        return created;
-    }
-
-    public void setCreated(Date created) {
-        this.created = created;
-    }
 }
---
+++ todo-api/src/test/java/com/kazys/todoapi/TodoTest.java
@@ -0,0 +1,4 @@
+package com.kazys.todoapi;
+
+public class TodoTest {
+}
